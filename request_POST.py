a20a56653ff578b091934305960da0176321358a
robin <robin.xw@hotmail.com>
2019-12-30 18:21:09 -0800

    fix print

--- request_POST.py
+++ request_POST.py
@@ -19,13 +19,17 @@ response = requests.post(url, files=files, data=data)
 result = response.json()
 task_ID = result['task_ID']
 status = result['status']
-res_url = url + '/result/{}'.format(task_ID)
+res_url = url + '/results/{}'.format(task_ID)
+idx = 1
 while status == 'In Progress':
-    time.sleep(2)
+    time.sleep(3)
     response_res = requests.get(res_url)
     status = response_res.json()['status']
-    print('\ttask status: {}'.format(status))
+    status_display = '\tTask status: {}... {}s has passed'.format(status, (idx * 3))
+    print(status_display, end="\r")
+    idx += 1
     if status == 'Completed':
         result_json = response_res.json()['result']
         with open('get_result.json', 'w') as outf:
-            outf.write(json.dumps(result_json, indent=4, sort_keys=True, ensure_ascii=False)) 
+            outf.write(result_json)
+        print("\r{}, result JSON save to {}".format(status_display, 'get_result.json'))
